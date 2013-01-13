6b054177f7a0a6d9028db919db2c46e0d64f2b27
Alex <alex_lee@outlook.com>
2013-01-14 00:08:18 +0800

    dsf

--- target/classes/db/insert.sql
+++
@@ -1,7 +0,0 @@
-insert into posts (name,content,create_time,update_time)values(' CKEditor logo 可视化HTML编辑器 CKEditor ','<div>
-			
-			<p>CKEditor是新一代的<a href="http://www.oschina.net/p/fckeditor">FCKeditor</a>，是一个重新开发的版本。CKEditor是全球最优秀的网页在线文字编辑器之一，因其惊人的性能与可扩展性而广泛的被运用于各大网站。</p> 
-<p>可配合使用的扩展有 <a href="http://www.oschina.net/p/kcfinder">文件管理器KCFinder</a></p> 
-<p><img alt="" src="http://www.oschina.net/uploads/img/201007/18130009_BkZR.png"></p> 
-<p>在线演示：<a target="_blank" href="http://ckeditor.com/demo">http://ckeditor.com/demo</a><br> <br></p>
-		</div>',4242342,4534532423);
