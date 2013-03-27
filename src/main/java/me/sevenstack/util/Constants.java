ed8a15bb06117c87df4dd9ffea7b90e1d8fa61d7
Alex <alex_lee@outlook.com>
2013-03-27 21:10:37 +0800

    op

--- src/main/java/me/sevenstack/util/Constants.java
+++ src/main/java/me/sevenstack/util/Constants.java
@@ -3,7 +3,7 @@ package me.sevenstack.util;
 import java.util.regex.Pattern;
 
 public class Constants {
-    public static String ENV = "dev";
+    public static String ENV = "beta";
     public static String USER_SESSION = "session-me.sevenstack.web.model.user";
     public static Byte STATUS_DEL = new Byte("1");
     public static Byte STATUS_NORMAL = new Byte("0");
